101376f02e009c586d6d1ba487d5f746fb9523e8
Sanjeet Uppal <sanjeet.uppal92@gmail.com>
2017-08-21 10:26:00 -0700

    Add migration configuration

--- knexfile.js
+++ knexfile.js
@@ -3,15 +3,24 @@ module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://localhost/eka_dev',
+    migrations: {
+      tableName: 'knex_migrations'
+    }
   },
   
   test: {
     client: 'pg',
-    connection: 'postgres://localhost/eka_test'
+    connection: 'postgres://localhost/eka_test',
+    migrations: {
+      tableName: 'knex_migrations'
+    }
   },
   
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
+    migrations: {
+      tableName: 'knex_migrations'
+    }
   }
 };
