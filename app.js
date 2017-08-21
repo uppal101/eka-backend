03b1bc03ded485f6a03973297d17f9fb782f627a
Sanjeet Uppal <sanjeet.uppal92@gmail.com>
2017-08-21 09:44:45 -0700

    Allow cors

--- app.js
+++ app.js
@@ -3,12 +3,28 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
+const cors = require('cors');
 const usersRoute = require('./routes/users');
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
 
+const allowCrossDomain = function (req, res, next) {
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+  if (req.method == 'OPTIONS') {
+    res.send(200);
+  } else {
+    next();
+  }
+};
+
+app.use(allowCrossDomain);
+
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
